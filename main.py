c110ec70856e978ce0d94a758208a12c81f958d3
dsinghrathore <digvijaysra@gmail.com>
2024-04-12 03:28:12 -0500

    cleanup

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+"""
+
+NOTE: For easy testing TwilML Bins are being used, therefore this flask server will not be required
+    Instead stream.py includes a custom setup to handle the ws directly.
+
+
+"""
 from flask import Flask
 from flask_cors import CORS
 from flask_session import Session
 from flask_sock import Sock
 
-from stream import handle_stream
+# from stream import handle_stream
 from routes.voice import voice_bp
 
 app = Flask(__name__)
@@ -16,9 +23,9 @@ Session(app)
 
 app.register_blueprint(voice_bp, url_prefix='/voice')
 
-@socket.route('/stream')
-async def _stream(ws):
-    await handle_stream(ws)
+# @socket.route('/stream')
+# async def _stream(ws):
+#     await handle_stream(ws)
 
 if __name__ == '__main__':
     app.run(debug=True)
